8eb087be20d624f223bfec2236cb1538bbb5360f
seanarch <zhuqingyun@outlook.com>
2023-10-10 14:23:56 -0600

    Use serializer and class model to build crud operations

--- w2-drf/BookList/urls.py
+++ w2-drf/BookList/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('BookListAPI.urls')),
+    path('api/', include('BookListAPI.urls')),
 ]
--- w2-drf/BookListAPI/urls.py
+++ w2-drf/BookListAPI/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('books/', views.books)
+    # path('books/', views.books)
+    # path('books', views.BookList.as_view()),
+    # path('books/<int:pk>', views.Book.as_view())
+    path('menu-items', views.MenuItemsView.as_view()),
+    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view())
 ]
